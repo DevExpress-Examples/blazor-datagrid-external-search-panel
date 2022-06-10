6a2e05089cd34fc152fd2c5bec00719f8bdb1399
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2022-06-10 16:22:58 +0300

    upd

--- CS/DevExpressBlazorStarter/Data/WeatherForecast.cs
+++ CS/DevExpressBlazorStarter/Data/WeatherForecast.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace DevExpressBlazorStarter.Data
 {
     public class WeatherForecast
@@ -10,6 +8,6 @@ namespace DevExpressBlazorStarter.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
